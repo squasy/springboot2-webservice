f99d240b048487faea9e003010c4ba9fa2afe23f
squasy <squasy@gmail.com>
2022-11-10 13:15:31 +0900

    gitignore restart

---
+++ src/main/java/com/squasy/example/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.squasy.example.springboot.web.dto;
+
+import com.squasy.example.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
